5bb189379d00205384c4285a6c21ac2201893e45
Eric Guinois <Eric.Guinois@gatas.ca>
2023-12-09 07:20:24 +0200

    logging customization

--- modules/logging_config.py
+++ modules/logging_config.py
@@ -6,11 +6,18 @@ def setup_logging(loglevel):
     if loglevel is None:
         loglevel = os.environ.get("SD_WEBUI_LOG_LEVEL")
 
+    #Hardcode default to INFO
+    if loglevel is None:
+        loglevel = "INFO"
+
     if loglevel:
         log_level = getattr(logging, loglevel.upper(), None) or logging.INFO
         logging.basicConfig(
             level=log_level,
-            format='%(asctime)s %(levelname)s [%(name)s] %(message)s',
+            format='%(asctime)s [%(levelname)-9s][%(name)-20s] %(message)s',
             datefmt='%Y-%m-%d %H:%M:%S',
         )
+    #Remove logs for httpx, too much infos
+    logging.getLogger("httpx").setLevel(logging.WARNING)
+
 
